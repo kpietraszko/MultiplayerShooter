e50da1b547982686ed187c9671dc2734b11ff39e
Krzysztof Pietraszko <krzysztof@pietraszko.cba.pl>
2017-08-25 20:21:17 +0200

    First commit

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Client : MonoBehaviour
+{
+	private const int serverPort = 8888;
+	private int ClientSocketId;
+	private int ConnectionId;
+	private byte Error;
+	private int ReliableChannelId;
+	private int SocketId;
+	private void Connect()
+	{
+		NetworkTransport.Init();
+		var config = new ConnectionConfig();
+		ReliableChannelId = config.AddChannel(QosType.Reliable);
+		var topology = new HostTopology(config, 8);
+		SocketId = NetworkTransport.AddHost(topology, 0);
+		ConnectionId = NetworkTransport.Connect(SocketId, "127.0.0.1", serverPort, 0, out Error);
+	}
+
+	private void Start()
+	{
+		Connect();
+	}
+
+	private void Update()
+	{
+		PollConnection();
+	}
+	void PollConnection()
+	{
+		byte[] recBuffer = new byte[1024];
+		int hostId;
+		int connectionId;
+		int channelId;
+		int receivedSize;
+		byte error;
+		var networkEvent = NetworkTransport.Receive(out hostId, out connectionId, out channelId, recBuffer, recBuffer.Length * sizeof(byte), out receivedSize, out error);
+		switch (networkEvent)
+		{
+		case NetworkEventType.DataEvent:
+			break;
+		case NetworkEventType.ConnectEvent:
+			Debug.Log($"Connect event at frame {Time.frameCount}");
+			break;
+		case NetworkEventType.DisconnectEvent:
+			break;
+		case NetworkEventType.Nothing:
+			break;
+		case NetworkEventType.BroadcastEvent:
+			break;
+		default:
+			break;
+		}
+	}
+}
---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Server : MonoBehaviour
+{
+	private int ReliableChannelId;
+	private int SocketId;
+	private int ClientSocketId;
+	private byte Error;
+
+	private void Start()
+	{
+		NetworkTransport.Init();
+		var config = new ConnectionConfig();
+		ReliableChannelId = config.AddChannel(QosType.Reliable);
+		var topology = new HostTopology(config, 8);
+		SocketId = NetworkTransport.AddHost(topology, 8888);
+	}
+
+	private void Update()
+	{
+		PollConnection();
+	}
+	void PollConnection()
+	{
+		byte[] recBuffer = new byte[1024];
+		int hostId;
+		int connectionId;
+		int channelId;
+		int receivedSize;
+		byte error;
+		var networkEvent = NetworkTransport.Receive(out hostId, out connectionId, out channelId, recBuffer, recBuffer.Length * sizeof(byte), out receivedSize, out error);
+		switch (networkEvent)
+		{
+		case NetworkEventType.DataEvent:
+			break;
+		case NetworkEventType.ConnectEvent:
+			Debug.Log($"Connect event at frame {Time.frameCount}");
+			break;
+		case NetworkEventType.DisconnectEvent:
+			break;
+		case NetworkEventType.Nothing:
+			break;
+		case NetworkEventType.BroadcastEvent:
+			break;
+		default:
+			break;
+		}
+	}
+}
