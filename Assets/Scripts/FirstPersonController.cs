b80394aa94a4efa95a0cda7b54e14713338c4796
kpietraszko <krzysztof@pietraszko.cba.pl>
2017-08-30 14:20:45 +0200

    minor friction tweaks, stop time ~390

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -10,8 +10,15 @@ public class FirstPersonController : MonoBehaviour
 	ControlsSO _controls;
 	[SerializeField]
 	float MaxSpeed = Mathf.Infinity;
+	[SerializeField]
+	float DragConstant = 0.1f;
 	IInput _input;
 	Rigidbody _rigidbody;
+	Vector3 Velocity; //debug
+	int StopTime; //debug
+	Vector3 Force; //debug
+	Vector3 Drag; //debug
+	System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
 	void Start()
 	{
 		_input = new PlayerInput(); //pass _controls here in the future
@@ -25,8 +32,29 @@ public class FirstPersonController : MonoBehaviour
 
 	void FixedUpdate()
 	{
-		var movement = _input.GetAxes(AxisType.Movement).normalized * ForceMultiplier;
-		if(_rigidbody.velocity.magnitude < MaxSpeed)
-			_rigidbody.AddForce(movement.x, 0, movement.y, ForceMode.Force);
+		if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+		{
+			sw.Start();
+		}
+
+		var movement = _input.GetAxes(AxisType.Movement).normalized;
+		Velocity = _rigidbody.velocity;
+		Force = new Vector3(movement.x, 0f, movement.y) * ForceMultiplier;
+		Drag = DragConstant * SignedVectorSquare(Velocity);
+		var netForce = Force - Drag;
+		if (_rigidbody.velocity.magnitude < MaxSpeed)
+			_rigidbody.AddForce(netForce, ForceMode.Force);
+
+		if (Mathf.Approximately(Velocity.x, 0f))
+		{
+			sw.Stop();
+			var time = (int)sw.ElapsedMilliseconds;
+			StopTime = time > 0 ? time : StopTime; //assign only if greater than 0
+			sw.Reset();
+		}
+	}
+	Vector3 SignedVectorSquare(Vector3 vector)
+	{
+		return Vector3.Scale(vector, new Vector3(Mathf.Abs(vector.x), Mathf.Abs(vector.y), Mathf.Abs(vector.z)));
 	}
 }
--- Assets/Scripts/QuadraticDrag.cs
+++ Assets/Scripts/QuadraticDrag.cs
@@ -8,6 +8,7 @@ public class QuadraticDrag : MonoBehaviour
 	[SerializeField]
 	float DragConstant = 1;
 	Rigidbody rb;
+	Vector3 drag;
 	void Start()
 	{
 		rb = GetComponent<Rigidbody>();
@@ -16,7 +17,8 @@ public class QuadraticDrag : MonoBehaviour
 	void FixedUpdate()
 	{
 		var velocity = rb.velocity;
-		var drag = DragConstant * Vector3.Scale(velocity,velocity); //constant * velocity^2
+		var dragAlong = DragConstant * Vector3.Scale(velocity,velocity); //constant * velocity^2
+		drag = dragAlong * -1;
 		rb.AddForce(drag, ForceMode.Force);
 	}
 
